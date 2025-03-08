c729ada8d83b1777ee0651e70f1991e24da96d2e
mdesai3112 <161511181+mdesai3112@users.noreply.github.com>
2025-03-08 15:41:54 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,21 @@ public class SimpleCalculatorTest {
 		int result = calc.minus(5, 3);
 		assertEquals("5 - 3 should be 2",2,result);
 	}
+	@Test
+	public void multiply() {
+	    SimpleCalculator calc = new SimpleCalculator();
+	    int result = calc.mulyiply(2, 3);
+	    assertEquals("2 * 3 should be 6", 6, result);
+	}
+	@Test
+	public void divide() {
+	    SimpleCalculator calc = new SimpleCalculator();
+	    int result = calc.divide(10, 2);
+	    assertEquals("10 / 2 should be 5", 5, result);
+	}
+	@Test(expected = ArithmeticException.class)
+    public void testDivideByZero_throwsArithmeticException() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.divide(5, 0);
+    }
 }
